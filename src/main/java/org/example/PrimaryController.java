c0e16aced92c15183d28d70907f4f247195fbced
bindas1 <bartek.binda46@gmail.com>
2021-04-10 23:51:44 +0200

    Basic login application window

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -1,12 +1,27 @@
+//package org.example;
+//
+//import java.io.IOException;
+//import javafx.fxml.FXML;
+//
+//public class PrimaryController {
+//
+//    @FXML
+//    private void switchToSecondary() throws IOException {
+//        App.setRoot("secondary");
+//    }
+//}
+
 package org.example;
 
-import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 
 public class PrimaryController {
+    @FXML private Text actiontarget;
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
     }
+
 }
